a1167b5cfd2531c230acc9c81d3b83898e071dcb
shernix <eduardo.santiago8@upr.edu>
2019-05-05 18:50:58 -0400

    LabeledDataset

--- Read_file.py
+++ Read_file.py
@@ -1,11 +1,11 @@
 import keras
 
 def read_file():
-    text=(open("/users/maria/github/artint/Dataset/Dataset.txt", encoding = "utf-8-sig").read().lower())
+    text=(open("/home/eduardo/github/AI_NLG/Info-bot/Dataset/Dataset.txt", encoding = "utf-8-sig").read().lower())
     words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' ')
     return words
 
 def readStarting():
-    text=(open("/users/maria/github/artint/Dataset/Greetings.txt", encoding = "utf-8-sig").read().lower())
+    text=(open("/home/eduardo/github/AI_NLG/Info-bot/Dataset/Greetings.txt", encoding = "utf-8-sig").read().lower())
     words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' ')
     return words
