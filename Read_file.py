7528560e3095de9d3f728ca0cf2091c5499b7375
Maria Marrero <maria.marrero13@upr.edu>
2019-05-05 19:13:01 -0400

    .

--- Read_file.py
+++ Read_file.py
@@ -1,11 +1,11 @@
 import keras
 
 def read_file():
-    text=(open("Dataset/Dataset.txt", encoding = "utf-8-sig").read().lower())
+    text=(open("/Users/maria/github/ArtInt/Dataset/Labeled_Dataset.txt", encoding = "utf-8-sig").read().lower())
     words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-./:;<=>?@\\^_`{|}~\t\n', lower=True, split=' ')
     return words
 
 def readStarting():
-    text=(open("Dataset/Greetings.txt", encoding = "utf-8-sig").read().lower())
+    text=(open("/Users/maria/github/ArtInt/Dataset/Greetings.txt", encoding = "utf-8-sig").read().lower())
     words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' ')
     return words
