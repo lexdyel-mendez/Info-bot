17250278216dc337582357ebe47c59f37247d073
Lexdyel Mendez Rios <lexdyel.mendez@upr.edu>
2019-05-05 22:54:39 -0400

    Fixed interface to take in consideration if you dont want a date.

--- Read_file.py
+++ Read_file.py
@@ -3,10 +3,11 @@ import keras
 def read_file():
     text=(open("Dataset/Labeled_Dataset.txt", encoding = "utf-8-sig").read().lower())
    #text=(open("Dataset/Labeled_Dataset.txt", encoding = "utf-8-sig").read().lower())
-    words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-./:;<=>?@\\^_`{|}~\t\n', lower=True, split=' ')
+    text = text.replace('.',' .')
+    words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-/:;<=>?@\\^_`{|}~\t\n', lower=True, split=' ')
     return words
 
 def readStarting():
     text=(open("Dataset/Greetings.txt", encoding = "utf-8-sig").read().lower())
-    words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' ')
+    words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split='\n')
     return words
