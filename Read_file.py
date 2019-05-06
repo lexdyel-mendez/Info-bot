34cfc28c81c02dc5bc50f0948dfe9e063231c05f
shernix <eduardo.santiago8@upr.edu>
2019-05-05 21:49:48 -0400

    Commit

--- Read_file.py
+++ Read_file.py
@@ -1,7 +1,7 @@
 import keras
 
 def read_file():
-    text=(open("/home/eduardo/github/AI_NLG/Info-bot/Dataset/Dataset.txt", encoding = "utf-8-sig").read().lower())
+    text=(open("/home/eduardo/github/AI_NLG/Info-bot/Dataset/Labeled_Dataset.txt", encoding = "utf-8-sig").read().lower())
     words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' ')
     return words
 
