1f7bf3eb80468ad6fdd4860f702cd44ca28a48ea
shernix <eduardo.santiago8@upr.edu>
2019-05-05 23:52:10 -0400

    added dot again

--- Read_file.py
+++ Read_file.py
@@ -2,8 +2,9 @@ import keras
 
 def read_file():
     text=(open("Dataset/Labeled_Dataset.txt", encoding = "utf-8-sig").read().lower())
-   #text=(open("Dataset/Labeled_Dataset.txt", encoding = "utf-8-sig").read().lower())
-    words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-./:;<=>?@\\^_`{|}~\t\n', lower=True, split=' ')
+    #text=(open("Dataset/Labeled_Dataset.txt", encoding = "utf-8-sig").read().lower())
+    text = text.replace('.',' .')
+    words = keras.preprocessing.text.text_to_word_sequence(text, filters='!"#$%&()*+,-/:;<=>?@\\^_`{|}~\t\n', lower=True, split=' ')
     return words
 
 def readStarting():
